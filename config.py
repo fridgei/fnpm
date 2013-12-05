15176d17ad92909ca2258ae4b8e511ccbde05592
Ian Fridge <ian.fridge@urbanairship.com>
2013-12-04 23:35:46 -0800

    add more shit

---
+++ config.py
@@ -0,0 +1,13 @@
+defaults = {
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:////Users/ianfridge/repos/fnpm/npm.db',
+    'UPSTREAM_REGISTRY': 'http://registry.npmjs.org',
+    'LOCAL_REGISTRY': 'http://localhost:8080',
+    'LOCAL_PACKAGE_PATH': '/Users/ianfridge/repos/fnpm/packages'
+}
+
+test_defaults = {
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:////Users/ianfridge/repos/fnpm/test.db',
+    'UPSTREAM_REGISTRY': 'http://registry.npmjs.org',
+    'LOCAL_REGISTRY': 'http://localhost:8080',
+    'LOCAL_PACKAGE_PATH': '/Users/ianfridge/repos/fnpm/test_packages'
+}
